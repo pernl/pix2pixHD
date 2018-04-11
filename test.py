24507ca37fc7777f473df299d2549da5154fcc43
Per Nilsson Lundberg <per.nilsson-lundberg@zenuity.com>
2018-04-11 15:23:13 +0200

    Added option to output labels

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-### Copyright (C) 2017 NVIDIA Corporation. All rights reserved. 
+### Copyright (C) 2017 NVIDIA Corporation. All rights reserved.
 ### Licensed under the CC BY-NC-SA 4.0 license (https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode).
 import os
 from collections import OrderedDict
@@ -27,8 +27,11 @@ for i, data in enumerate(dataset):
     if i >= opt.how_many:
         break
     generated = model.inference(data['label'], data['inst'])
-    visuals = OrderedDict([('input_label', util.tensor2label(data['label'][0], opt.label_nc)),
-                           ('synthesized_image', util.tensor2im(generated.data[0]))])
+    if opt.output_labels:
+        visuals = OrderedDict([('_input_label', util.tensor2label(data['label'][0], opt.label_nc)),
+                               ('img', util.tensor2im(generated.data[0]))])
+    else:
+        visuals = OrderedDict([('img', util.tensor2im(generated.data[0]))])
     img_path = data['path']
     print('process image... %s' % img_path)
     visualizer.save_images(webpage, visuals, img_path)
